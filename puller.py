8a52995131bf41b3e1cd1df968d7a02f633df0c7
philbowman <47841859+philbowman@users.noreply.github.com>
2022-06-08 21:10:55 +0300

    Update puller.py

--- puller.py
+++ puller.py
@@ -2,12 +2,98 @@ import sys
 import subprocess
 import os
 import csv
+from pathlib import Path
+from datetime import datetime
+import shutil
+import git
 
 STUDENTWORK_DIR = "studentwork/"
+COMPARE50_DIR = "compare50"
+COURSE_ID = "958"
 
 if not os.path.exists(STUDENTWORK_DIR):
     subprocess.run("mkdir " + STUDENTWORK_DIR, shell=True)
 
+
+def run_compare50(slug, foldername, archive_path):
+    assignment_path = STUDENTWORK_DIR + foldername
+    archive_folder = str(Path().resolve().parent) + "/source50/" + archive_path
+    output_dir = f"{assignment_path}/{COMPARE50_DIR}"
+    if os.path.exists(assignment_path + "/distro"):
+        distro = " -d "+ assignment_path + "/distro/*.c"
+    else:
+        distro = ""
+    if os.path.exists(output_dir):
+        print("archiving old compare50 output...")
+        archive_dir = output_dir+"_archived_" + datetime.now().strftime("%Y%m%d")
+        if os.path.exists(archive_dir):
+            shutil.rmtree(archive_dir)
+        os.rename(output_dir, archive_dir)
+
+    if os.path.exists(archive_folder):
+        print(f"slug: {slug}\nfoldername: {foldername}\narchive_path: {archive_path}")
+        print(f"compare50 {assignment_path}/*/*.c -a {archive_folder}/*/*.c -o {output_dir}{distro}")
+        subprocess.run(f"compare50 {assignment_path}/*/*.c -a {archive_folder}/*/*.c -o {output_dir}{distro}", shell=True)
+    else:
+        print("no archive found")
+        print(f"compare50 {assignment_path}/*/*.c -o {output_dir}{distro}")
+
+        subprocess.run(f"compare50 {assignment_path}/*/*.c -o {output_dir}{distro}", shell=True)
+
+
+def list_submissions(slugs):
+    print(slugs)
+    if os.path.exists(STUDENTWORK_DIR + "list.html"):
+        archive_file = STUDENTWORK_DIR + "list_archived_" + datetime.now().strftime("%Y%m%d") + ".html"
+        if os.path.exists(archive_file):
+            os.remove(archive_file)
+        os.rename(STUDENTWORK_DIR + "list.html", archive_file)
+    with open(STUDENTWORK_DIR + "list.html", "a") as file:
+        for slug in slugs:
+            assignment_path = STUDENTWORK_DIR + slug['folder']
+            print(assignment_path)
+            if os.path.exists(assignment_path):
+
+                
+                if os.path.exists(assignment_path + "/" + COMPARE50_DIR):
+                    file.write(f"<h1>{slug['folder']}</h1>")
+                    file.write("<p>" + slug['slug'] + "</p>")
+                    file.write("<p><a href=\"file://" + os.path.abspath(assignment_path + "/" + COMPARE50_DIR) + "/index.html\" target=\"_blank\">" + "compare50" + "</a> | ")
+                    file.write(f"<a href=\"https://submit.cs50.io/courses/{COURSE_ID}/{slug['slug']}\" target=\"_blank\">me50</a> | \n")
+                    file.write(f"<a href=\"file://{os.path.abspath(assignment_path)}\" target=\"_blank\">folder</a></p>")
+                else:
+                    file.write("<h1>" + slug['folder'] + "</h1>" + "\n")
+                with open("students.csv") as students:
+                    file.write("<ol>")
+                    for person in csv.DictReader(students):
+                        
+                        if os.path.exists(assignment_path + "/" + person['folder']):
+                            student_folder = assignment_path + "/" + person['folder']
+                            file.write(f"<li><a href=\"https://github.com/me50/{person['username']}/tree/{slug['slug']}\" target=\"_blank\">")
+                            file.write(person['folder'] + "</a> | ")
+                            file.write(f"<a href=\"file://{os.path.abspath(student_folder)} \"target=\"_blank\">folder</a>")
+
+                            repo = git.Repo(student_folder)
+                            branch = repo.head.reference
+                            print(student_folder)
+                            submitted = False
+                            for commit in repo.iter_commits():
+                                if "submit50" in commit.message:
+                                    submitted = True
+                                    submit_commit = commit
+                                    break
+                            if "check50" in branch.commit.message:
+                                file.write(" [check50]\n")
+                                if submitted:
+                                    file.write(f"<a href=\"https://github.com/me50/{person['username']}/tree/{commit.tree}\" target=\"_blank\">submit50</a>")
+                            
+                            file.write("</li>\n")
+
+                        else:
+                            file.write("<li><s>" + person['folder'] + "</s></li>\n")
+                    file.write("</ol>")
+                file.write("\n")
+    print("file://" + os.path.abspath(STUDENTWORK_DIR + "list.html"))
 
 def get_projects(slug, foldername):
     print(f"fetching student work for {slug}")
@@ -38,23 +124,32 @@ def get_projects(slug, foldername):
         print("students.csv does not exist")
 
 def get_assignments():
+    foldernames = []
     try:
         #iterate over list of assignments in slugs.csv
         with open('slugs.csv') as slugs:
             for slug in csv.DictReader(slugs):
                 #use user-defined local folder name
+                slug_arr = slug['slug'].split('/')
+
+                #create folder names for "more" and "less" assignments
+                if slug_arr[-1] == "less" or slug_arr[-1] == "more":
+                    foldername = f"{slug_arr[-2]}_{slug_arr[-1]}"
+                    archive_path = f"{slug_arr[-2]}"
+                else:
+                    foldername = slug_arr[-1]
+                    archive_path = slug_arr[-1]
+
+                #override the foldername
                 if slug['folder']:
                     foldername = slug['folder']
-                
-                else:
-                    slug_arr = slug['slug'].split('/')
-                    #create folder names for "more" and "less" assignments
-                    if slug_arr[-1] == "less" or slug_arr[-1] == "more":
-                        foldername = f"{slug_arr[-2]}_{slug_arr[-1]}"
-                    else:
-                        foldername = slug_arr[-1]
 
                 get_projects(slug['slug'], foldername)
+                run_compare50(slug['slug'], foldername, archive_path)
+                foldernames.append({'folder': foldername, 'slug': slug['slug']})
+
+
+        list_submissions(foldernames)
     
     except FileNotFoundError:
         print("slugs.csv does not exist")
